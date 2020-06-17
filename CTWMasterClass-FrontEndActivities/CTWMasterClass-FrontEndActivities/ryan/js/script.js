5c66e81f5b29036beb02ddcafcfdca53a3b58836
Ryan Thom <thomrya22@outlook.com>
2020-06-17 14:03:24 -0500

    drew grid

--- CTWMasterClass-FrontEndActivities/CTWMasterClass-FrontEndActivities/ryan/js/script.js
+++ CTWMasterClass-FrontEndActivities/CTWMasterClass-FrontEndActivities/ryan/js/script.js
@@ -1,5 +1,33 @@
-﻿"use strict"
+﻿const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+const CANVAS_WIDTH = c.width;
+const CANVAS_THIRD = CANVAS_WIDTH / 3;
+
+start();
+
+
 function start() {
-    const c = document.getElementById("myCanvas");
-    const ctx = c.getContext("2d");
+    drawLines();
+}
+
+function drawLines() {
+    ctx.beginPath();
+    ctx.moveTo(CANVAS_THIRD, 0);
+    ctx.lineTo(CANVAS_THIRD, CANVAS_WIDTH);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(CANVAS_THIRD * 2, 0);
+    ctx.lineTo(CANVAS_THIRD * 2, CANVAS_WIDTH);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, CANVAS_THIRD);
+    ctx.lineTo(CANVAS_WIDTH, CANVAS_THIRD);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, CANVAS_THIRD * 2);
+    ctx.lineTo(CANVAS_WIDTH, CANVAS_THIRD * 2);
+    ctx.stroke();
 }
